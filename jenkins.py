dd2958af046f62248e5ba62a132d046a5bf03fe0
peernitzanim <peer.vetzler@nitzanim.tech>
2024-09-14 02:44:23 +0300

    Update jenkins.py

--- jenkins.py
+++ jenkins.py
@@ -36,5 +36,5 @@ def s3_delete(name, myname):
 def s3_update(name, myname, filename):
     print(filename)
     jenkins_info = {'action': 'update', 'name': name, 'myname': myname, 'filename': filename}
-    #action_jenkins_s3(jenkins_info)
+    action_jenkins_s3(jenkins_info)
 
