2fcf16d9a6bd69e2c10307815b319e48c1ba4e6f
peernitzanim <peer.vetzler@nitzanim.tech>
2024-09-14 02:17:19 +0300

    Update jenkins.py

--- jenkins.py
+++ jenkins.py
@@ -1,5 +1,5 @@
 from ec2 import action_jenkins_ec2
-import s3
+from s3 import action_jenkins_s3
 import route53
 
 
@@ -18,3 +18,22 @@ def ec2_delete(name, myname):
 def ec2_update(name, myname, status):
     jenkins_info = {'action': 'update', 'name': name, 'myname': myname, 'status': status}
     action_jenkins_ec2(jenkins_info)
+
+
+
+def s3_create(name, myname, public):
+    jenkins_info = {'action': 'create', 'name': name, 'myname': myname, 'public': public}
+    action_jenkins_s3(jenkins_info)
+
+def s3_list(myname):
+    jenkins_info = {'action': 'list', 'myname': myname}
+    action_jenkins_s3(jenkins_info)
+
+def s3_delete(name, myname):
+    jenkins_info = {'action': 'delete', 'name': name, 'myname': myname}
+    action_jenkins_s3(jenkins_info)
+
+def s3_update(name, myname, filename):
+    jenkins_info = {'action': 'update', 'name': name, 'myname': myname, 'filename': filename}
+    action_jenkins_s3(jenkins_info)
+
