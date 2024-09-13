84a8bcc0ef2f55411479257b0e3bfb84f1fe34e4
peernitzanim <peer.vetzler@nitzanim.tech>
2024-09-14 01:41:10 +0300

    Update jenkins.py

--- jenkins.py
+++ jenkins.py
@@ -10,3 +10,11 @@ def ec2_create(name, myname, ami, instance_type):
 def ec2_list(myname):
     jenkins_info = {'action': 'list', 'myname': myname}
     action_jenkins_ec2(jenkins_info)
+
+def ec2_delete(name, myname):
+    jenkins_info = {'action': 'delete', 'name': name, 'myname': myname}
+    action_jenkins_ec2(jenkins_info)
+
+def ec2_update(name, myname, status):
+    jenkins_info = {'action': 'update', 'name': name, 'myname': myname, 'status': status}
+    action_jenkins_ec2(jenkins_info)
