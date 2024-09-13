897903b729f001c17380cf7d2af4def4615c51a6
peernitzanim <peer.vetzler@nitzanim.tech>
2024-09-13 19:44:19 +0300

    Update __init__.py

--- ec2/__init__.py
+++ ec2/__init__.py
@@ -35,9 +35,7 @@ def action_jenkins_ec2(jenkins_info):
     if 'list' in jenkins_info['action']:
         return list_instances(jenkins_info['myname'], "Jenkins")
     elif 'create' in jenkins_info['action']:
-        print(jenkins_info)
-        return 5
-        #return create_instance(jenkins_info['name'], jenkins_info['myname'], "Jenkins", jenkins_info['ami'], jenkins_info['instance_type'])
+        return create_instance(jenkins_info['name'], jenkins_info['myname'], "Jenkins", jenkins_info['ami'], jenkins_info['instance_type'])
     elif 'delete' in jenkins_info['action']:
         return vm_exists(jenkins_info['name'], jenkins_info['myname'], 'delete', "Jenkins")
     elif 'update' in jenkins_info['action']:
